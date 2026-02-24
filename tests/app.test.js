3fe38518bbd793fb7b0acc9e9aa0be0ad4f4b511
sowmiya-projects <sowmithanam03@gmail.com>
2026-02-24 13:10:57 +0530

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,7 +5,7 @@ describe('GET /', () => {
   it('returns 200 with message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.body.message).toBe('Hello from GCP Pipeline!');
+    expect(res.body.message).toBe('Hello from GCP Pipeline-2!');
   });
 });
 describe('GET /health', () => {
